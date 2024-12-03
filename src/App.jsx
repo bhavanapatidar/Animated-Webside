ca2be3f80311450984f89d71216a3f0d2d7a9090
bhavanapatidar <bhavana.patidar2708@gmail.com>
2024-12-03 22:59:32 +0530

    demo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import About from './components/About';
 import Heros from './components/Heros';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar/>
       <Heros/>
       <About/>
     </main>
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useGSAP } from '@gsap/react';
 import { gsap } from 'gsap/gsap-core';
 import { ScrollTrigger } from 'gsap/all';
+import AnimatedTitle from './AnimatedTitle';
 
 gsap.registerPlugin(ScrollTrigger)
 
@@ -31,10 +32,10 @@ const About = () => {
     <div id='about' className='min-h-screen w-screen'>
         <div className='relative mb-8 mt-36 flex flex-col items-center gap-5'>
             <h2 className='font-general text-sm uppercase md:text-[10px]'>Welcome to Zentry</h2>
-            <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]  '>
 
-                Disc<b>o</b>ver the World's <br /> l<b>a</b>rgest shared adventure
-            </div>
+                <AnimatedTitle title = "Disc<b>o</b>ver the World's <br /> l<b>a</b>rgest shared adventure" containerClass = "mt-5 !text-black text-center"/>
+
+           
             <div className='about-subtext'>
                 <p> The Game of Games begins-your life, now an epic MMORPG</p>
                 <p>
