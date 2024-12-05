e1b245658c5468bf59e3d7b4fcb6fd2cd7e0d47a
bhavanapatidar <bhavana.patidar2708@gmail.com>
2024-12-06 00:37:58 +0530

    more components

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import About from './components/About';
 import Heros from './components/Heros';
 import Navbar from './components/Navbar';
-
+import Features from './components/Features';
+import Story from './components/Story';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Navbar/>
       <Heros/>
       <About/>
+      <Features/>
+      <Story/>
+      <Contact/>
+      <Footer/>
     </main>
 
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,43 @@
 import React, { useEffect } from 'react'
 import { useRef, useState } from 'react'
-import { TiLocationArrow } from 'react-icons/ti';
-import Button from './Button.jsx';
-import { useWindowScroll } from 'react-use';
+import { TiLocationArrow } from 'react-icons/ti'
+import Button from './Button.jsx'
+import { useWindowScroll } from 'react-use'
+import { gsap } from 'gsap/gsap-core'
+
 
 const navItems = ['Nexus', 'Vault', 'Prologue', 'About', 'Contact'];
 
 const Navbar = () => {
     const [isAudioPlaying, setIsAudioPlaying] = useState(false);
     const [isIndicatorActive, setIsIndicatorActive] = useState(false);
+    const [lastScrollY, setLastScrollY] = useState(0);
+    const [isNavVisible, setIsNavVisible] = useState(true);
 const navContainerRef = useRef(null);
 const audioElementRef = useRef(null);
 
-const{currentScrollY} = useWindowScroll();
-use
+const{y:currentScrollY} = useWindowScroll();
+useEffect(() => {
+    if(currentScrollY === 0){
+        setIsNavVisible(true);
+        navContainerRef.current.classList.remove('floating-nav');
+    } else if(currentScrollY > lastScrollY){
+        setIsNavVisible(false);
+        navContainerRef.current.classList.add('floating-nav');
+    }else if (currentScrollY < lastScrollY){
+        setIsNavVisible(true);
+        navContainerRef.current.classList.add('floating-nav');
+    }
+    setLastScrollY(currentScrollY);
+}, [currentScrollY, lastScrollY])
 
+useEffect(() => { 
+    gsap.to(navContainerRef.current,  {
+        y: isNavVisible ? 0 : -100,
+        opacity : isNavVisible ? 1 :0,
+        duration: 0.2,
+    }) 
+}, [isNavVisible])
 
 const toggleAudioIndicator = () => {
     setIsAudioPlaying((prev) => !prev);
